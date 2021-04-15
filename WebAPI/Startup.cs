c01cc96ab05d2130403d3ae0e3568f15d5f04220
Aykut Cihan Sevim <aykutcihansevim@hotmail.com>
2021-04-15 17:27:09 +0300

    Yazılım Geliştirici Yetiştirme Kampı - 17. Ders Sonu

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -77,6 +77,7 @@ namespace WebAPI
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseCors(builder=>builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
             app.UseHttpsRedirection();
 
             app.UseRouting();
